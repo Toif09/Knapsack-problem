852de370e304ad34b0a90a4632282a54a1609dd6
TOIF MUHAYAT <74441816+Toif09@users.noreply.github.com>
2023-09-15 22:53:03 +0700

    first commit

---
+++ uasknapsakgreedy/src/uasknapsakgreedy/Uasknapsakgreedy.java
@@ -0,0 +1,17 @@
+package uasknapsakgreedy;
+
+public class Uasknapsakgreedy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        login log = new login();
+        log.setVisible(true);
+        log.pack();
+        log.setLocationRelativeTo(null);
+        log.setDefaultCloseOperation(login.EXIT_ON_CLOSE);
+    }
+    
+}
